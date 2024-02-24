82a04dc1f2f2311cc2320890dcd352191660e9e4
Wexoss <wexus12@interia.pl>
2024-02-24 18:11:54 +0100

    poprawa zadania PolscyNoblisciV1

--- zadania dodatkowe/zadania-dodatkowe/src/components/PolscyNoblisciV1.jsx
+++ zadania dodatkowe/zadania-dodatkowe/src/components/PolscyNoblisciV1.jsx
@@ -4,15 +4,14 @@ export const PolscyNoblisciV1 = () => {
   
   return (
     <>
-      {nobelPrizeWinners.map((noblista,index) => 
-      (noblista.nationality === "Polish" &&
-      <div className="noblista" key={index}>
+      {nobelPrizeWinners.map((noblista) => 
+      <div className="noblista" key={noblista.name}>
         <h3>{noblista.name}</h3>
         <p>Nationality: {noblista.nationality}</p>
         <p>Category: {noblista.category}</p>
         <p>Number of achievements: {noblista.achievements.length}</p>
       </div>
-      ))}
+      )}
     </>
   );
 };
