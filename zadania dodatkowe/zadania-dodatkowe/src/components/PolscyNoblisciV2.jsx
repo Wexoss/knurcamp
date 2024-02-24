dcd555da896fbead17eabdb8d49df3930a8092ef
Wexoss <wexus12@interia.pl>
2024-02-24 18:18:05 +0100

    poprawa zadania PolscyNoblisciV2

--- zadania dodatkowe/zadania-dodatkowe/src/components/PolscyNoblisciV2.jsx
+++ zadania dodatkowe/zadania-dodatkowe/src/components/PolscyNoblisciV2.jsx
@@ -1,44 +1,44 @@
 import { nobelPrizeWinners } from "../assets/nobilsci";
 
 export const PolscyNoblisciV2 = () => {
+  const tylkoPolscyNoblisci = nobelPrizeWinners.filter(
+    (dane) => dane.nationality === "Polish"
+  );
+  const wszyscyOproczPolakow = nobelPrizeWinners.filter(
+    (dane) => dane.nationality !== "Polish"
+  );
   return (
     <>
       <h1>Polscy Nobliści</h1>
-      {nobelPrizeWinners.map(
-        (noblista, index) =>
-          noblista.nationality === "Polish" && (
-            <div className="noblista" key={index}>
-              <h3>{noblista.name}</h3>
-              <p>Nationality: {noblista.nationality}</p>
-              <p>Category: {noblista.category}</p>
-              <p>Number of achievements: {noblista.achievements.length}</p>
+      {tylkoPolscyNoblisci.map((noblista) => (
+        <div className="noblista" key={noblista.name}>
+          <h3>{noblista.name}</h3>
+          <p>Nationality: {noblista.nationality}</p>
+          <p>Category: {noblista.category}</p>
+          <p>Number of achievements: {noblista.achievements.length}</p>
 
-              {noblista.achievements.map((aczivment, index) => (
-                <ul key={index}>
-                  <li>{aczivment}</li>
-                </ul>
-              ))}
-            </div>
-          )
-      )}
+          {noblista.achievements.map((aczivment, index) => (
+            <ul key={index}>
+              <li>{aczivment}</li>
+            </ul>
+          ))}
+        </div>
+      ))}
       <h1>Nobliści</h1>
-      {nobelPrizeWinners.map(
-        (noblista, index) =>
-          noblista.nationality != "Polish" && (
-            <div className="noblista" key={index}>
-              <h3>{noblista.name}</h3>
-              <p>Nationality: {noblista.nationality}</p>
-              <p>Category: {noblista.category}</p>
-              <p>Number of achievements: {noblista.achievements.length}</p>
+      {wszyscyOproczPolakow.map((noblista) => (
+        <div className="noblista" key={noblista.name}>
+          <h3>{noblista.name}</h3>
+          <p>Nationality: {noblista.nationality}</p>
+          <p>Category: {noblista.category}</p>
+          <p>Number of achievements: {noblista.achievements.length}</p>
 
-              {noblista.achievements.map((aczivment, index) => (
-                <ul key={index}>
-                  <li>{aczivment}</li>
-                </ul>
-              ))}
-            </div>
-          )
-      )}
+          {noblista.achievements.map((aczivment, index) => (
+            <ul key={index}>
+              <li>{aczivment}</li>
+            </ul>
+          ))}
+        </div>
+      ))}
     </>
   );
 };
