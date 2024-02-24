8f997b6fa6a15babf7de89d4d67a8132030f58e6
Wexoss <wexus12@interia.pl>
2024-02-24 18:31:30 +0100

    poprawa zadania ListaKsiazekV1

--- zadania dodatkowe/zadania-dodatkowe/src/components/ListaKsiazekV1.jsx
+++ zadania dodatkowe/zadania-dodatkowe/src/components/ListaKsiazekV1.jsx
@@ -3,25 +3,25 @@ import { useState } from "react";
 
 export const ListaKsiazekV1 = () => {
   const [ksiazka, setKsiazka] = useState("");
+  const szukaneKsiazki = books.filter((book) => book.match(ksiazka));
+
   return (
     <>
       <input
         type="text"
         onChange={(e) => setKsiazka(new RegExp(e.target.value, "gi"))}
       />
-      {books.map((book, index) =>
-        ksiazka === "" ? (
-          <div key={index}>
-            <p>{book}</p>
-          </div>
-        ) : book.match(ksiazka) ? (
-          <div key={index}>
-            <p>{book}</p>
-          </div>
-        ) : (
-          null
-        )
-      )}
+      {ksiazka === ""
+        ? books.map((book, index) => (
+            <div key={index}>
+              <p>{book}</p>
+            </div>
+          ))
+        : szukaneKsiazki.map((book, index) => (
+            <div key={index}>
+              <p>{book}</p>
+            </div>
+          ))}
     </>
   );
 };
