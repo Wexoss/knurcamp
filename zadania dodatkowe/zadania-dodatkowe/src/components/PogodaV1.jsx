3f9441db025c1e83908856c9d99c3f9acc384c76
Wexoss <wexus12@interia.pl>
2024-02-24 21:06:20 +0100

    zmiany w PogodaV1, oby na lepsze ;s

--- zadania dodatkowe/zadania-dodatkowe/src/components/PogodaV1.jsx
+++ zadania dodatkowe/zadania-dodatkowe/src/components/PogodaV1.jsx
@@ -3,39 +3,27 @@ import { useEffect, useState } from "react";
 export const PogodaV1 = () => {
   const [latitude, setLatitude] = useState(52.52);
   const [longitude, setLongitude] = useState(13.41);
-  const [temperatura, setTemperatura] = useState("0");
-  const [predkoscWiatru, setPredkoscWiatru] = useState(null);
-  const [godzina, setGodzina] = useState(null);
-  const [updateLat, setUpdateLat] = useState(null);
-  const [updateLong, setUpdateLong] = useState(null);
+  const [updateLat, setUpdateLat] = useState();
+  const [updateLong, setUpdateLong] = useState();
   const [czekamy, setCzekamy] = useState(true)
+  const [danePogodowe, setDanePogodowe] = useState()
 
-  const danePogodowe = async (lat, long) => {
+  const pobierzDanePogodowe = async (lat, long) => {
     const response = await fetch(
       `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`
     );
     const dane = await response.json();
-    const aktualnaTemperatura =
-      dane.current.temperature_2m + dane.current_units.temperature_2m;
-    const aktualnaPredkoscWiatru =
-      dane.current.wind_speed_10m + dane.current_units.wind_speed_10m;
-    const aktualnaGodzina = dane.current.time.split("T")[1];
-
-    setTemperatura(aktualnaTemperatura);
-    setPredkoscWiatru(aktualnaPredkoscWiatru);
-    setGodzina(aktualnaGodzina);
+    setDanePogodowe(dane)
     czekamyTrzySekundy()
-    czyPizga(dane.current.temperature_2m);
   };
 
   useEffect(() => {
     const interwal = setInterval(() => {
-      danePogodowe(latitude, longitude);
+      pobierzDanePogodowe(latitude, longitude);
     }, 60000);
 
-    danePogodowe(latitude, longitude);
+    pobierzDanePogodowe(latitude, longitude);
     return () => clearInterval(interwal);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [latitude, longitude]);
 
   const podmianka = () => {
@@ -43,13 +31,10 @@ export const PogodaV1 = () => {
     setLongitude(updateLong);
   };
 
-  const czyPizga = (temp) => {
-    setTimeout(() => {
-      if (temp <= -10) {
-        alert("ale pizga");
-      }
-    }, 3250);
-  };
+  useEffect(() => {
+    if (danePogodowe?.current?.temperature_2m === undefined || danePogodowe?.current?.temperature_2m > -10) return;
+    alert('ale pizga');
+  }, [danePogodowe]);
 
   const czekamyTrzySekundy = () => {
     setTimeout(() => {
@@ -58,17 +43,19 @@ export const PogodaV1 = () => {
     setCzekamy(true)
   };
 
-  return czekamy ? (
-    <p>LOADING DATA....</p>
-  ) : (
+  if (czekamy) {
+    return <p>LOADING DATA...</p>
+  }
+
+  return(
     <div
       style={{
         backgroundColor:
-          temperatura[0] === "-"
-            ? Number(temperatura.slice(0, 3)) <= -10
+        danePogodowe?.current?.temperature_2m < 0
+            ? danePogodowe?.current?.temperature_2m <= -10
               ? "blue"
               : "#DBF1FD"
-            : Number(temperatura.slice(0, 2)) >= 10
+            : danePogodowe?.current?.temperature_2m >= 10
             ? "red"
             : "pink",
       }}
@@ -89,9 +76,9 @@ export const PogodaV1 = () => {
       />
       <button onClick={podmianka}>Sprawdzam</button>
       <div>
-        <p>Temperatura: {temperatura}</p>
-        <p>Prędkość wiatru: {predkoscWiatru}</p>
-        <p>Godzina: {godzina}</p>
+        <p>Temperatura: {danePogodowe?.current?.temperature_2m + danePogodowe?.current_units?.temperature_2m}</p>
+        <p>Prędkość wiatru: {danePogodowe?.current?.wind_speed_10m + danePogodowe?.current_units?.wind_speed_10m}</p>
+        <p>Godzina: {danePogodowe?.current?.time.split("T")[1]}</p>
       </div>
     </div>
   );
